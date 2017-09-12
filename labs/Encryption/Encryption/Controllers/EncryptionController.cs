6105fa697bda40fc8cad238d9c018f709bd7f399
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-12 12:31:13 +0200

    Created and cleaned up new solution for web api meant for testing encryption

---
+++ labs/Encryption/Encryption/Controllers/EncryptionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Encryption.Controllers
+{
+    [Route("api/v1/encryption")]
+    public class EncryptionController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API is working";
+        }
+
+        [HttpGet("encrypt/{plaintext}")]
+        public string Get(int plaintext)
+        {
+            return $"Received: {plaintext}";
+        }
+    }
+}
