be72a9551f08156be3febe82fd9c3209c12b9a27
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-18 20:17:14 +0200

    Saving stuff

---
+++ labs/Encryption/Security/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Security.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hashing()
+        {
+            return View();
+        }
+
+        public IActionResult Encryption()
+        {
+            return View();
+        }
+    }
+}
